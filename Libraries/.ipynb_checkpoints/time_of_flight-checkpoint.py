f101bace91b50a8d254d8c721f44271037706275
SU-charlessnipp <167350861+SU-charlessnipp@users.noreply.github.com>
2024-11-13 16:05:38 -0800

    11/13 changes

--- Libraries/.ipynb_checkpoints/time_of_flight-checkpoint.py
+++ Libraries/.ipynb_checkpoints/time_of_flight-checkpoint.py
@@ -5,10 +5,15 @@ Last Updated: 10/21/24
 This class defines programs that can be run to determine the time of flight
 of output pulses in order to calibrate the ADC trigger offset
 
-
-TODO: add data saving that actually works
-
-
+cfg.expt = {
+    "n_avg": 500,
+    "ro_len": 13,
+    "pulse_len": 7,
+    "freq": 5981.7,
+    "phase": 0,
+    "gain": 1,
+    "trig_offset": 0
+}
 
 """
 from slab import experiment, AttrDict
@@ -16,7 +21,7 @@ from slab.experiment import Experiment # ?
 from qick.pyro import make_proxy
 from qick import *
 from qick.asm_v2 import AveragerProgramV2
-import os
+import os, h5py, json
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -27,12 +32,12 @@ class tof_pulse(AveragerProgramV2):
         
         ro_ch = cfg.soc.ro_ch
         gen_ch = cfg.soc.res_gen_ch
-        ro_len = cfg.readout.ro_len
-        pulse_len = cfg.readout.pulse_len
-        freq = cfg.readout.freq
-        phase = cfg.readout.phase
-        gain = cfg.readout.gain
-        trig_offset = cfg.readout.trig_offset
+        ro_len = cfg.expt.ro_len
+        pulse_len = cfg.expt.pulse_len
+        freq = cfg.expt.freq
+        phase = cfg.expt.phase
+        gain = cfg.expt.gain
+        trig_offset = cfg.expt.trig_offset
         
         self.declare_gen(ch=gen_ch, nqz=1)
         self.declare_readout(ch=ro_ch, length=ro_len)
@@ -51,7 +56,7 @@ class tof_pulse(AveragerProgramV2):
         cfg = AttrDict(cfg)
         ro_ch = cfg.soc.ro_ch
         gen_ch = cfg.soc.res_gen_ch
-        trig_offset = cfg.readout.trig_offset
+        trig_offset = cfg.expt.trig_offset
         
         self.send_readoutconfig(ch=ro_ch, name='ro', t=0)
         self.trigger(ros=[ro_ch], pins=[0], t=trig_offset, ddr4=True)
@@ -61,16 +66,10 @@ class tof_pulse(AveragerProgramV2):
 class time_of_flight(Experiment):
     # import config file, specify data path
     def __init__(self, path='', prefix='time_of_flight', config_file=None, liveplot_enabled=True, **kwargs):
-        if not os.path.exists(path):
-            print(f'Creating directory {path}')
-            os.makedirs(path)
         super().__init__(path=path, prefix=prefix, config_file=config_file, liveplot_enabled=liveplot_enabled, **kwargs)
-        print(self.fname)
-        print(self.prefix)
-        print(self.path)
         
     # run the experiment
-    def acquire(self, progress=False, save=True):
+    def acquire(self, progress=False):
         cfg = self.cfg
 
         ns_address = cfg.instrument_manager.ns_address
@@ -80,11 +79,10 @@ class time_of_flight(Experiment):
         soc, soccfg = make_proxy(ns_host=ns_address, ns_port=ns_port, proxy_name=proxy_name)
         
         prog = tof_pulse(soccfg=soccfg, reps=1, final_delay=None, cfg=cfg)
-        iq_list = prog.acquire_decimated(soc, soft_avgs=cfg.readout.n_avg)
+        iq_list = prog.acquire_decimated(soc, soft_avgs=cfg.expt.n_avg)
         t = prog.get_time_axis(ro_index=0)
         data = {"I": iq_list[0][:,0], "Q": iq_list[0][:,1], "time": t}
         self.data = data
-        # TODO: add data saving w/ h5 (after figuring out what Experiment saves by default)
         return data
 
     # plot results
@@ -101,15 +99,33 @@ class time_of_flight(Experiment):
         plt.legend()
         plt.ylabel("a.u.")
         plt.xlabel("us")
-        plt.title("Time of Flight\nFrequency: " + str(self.cfg.readout.freq))
+        plt.title("Time of Flight\nFrequency: " + str(self.cfg.expt.freq))
         plt.show()
-        if save: fig.savefig(self.path)
+        if save:
+            if not os.path.exists(self.path):
+                print(f'Creating directory {self.path}')
+                os.makedirs(self.path)
+            fig.savefig(self.path)
         return
 
-    def savedata(self):
-        with self.datafile() as f:
-            for k, d in data.items():
-                f.add(k, np.array(d))
+    def savedata(self): # I should really find a better way to do all this that is more friendly with different ways of saving/storing data and configs in SLab
+        if not os.path.exists(self.path):
+            print(f'Creating directory {self.path}')
+            os.makedirs(self.path)
+
+        # save data in h5
+        with h5py.File(self.path + "data.h5", "w") as h5file:
+            for key, value in self.data.items():
+                h5file.create_dataset(key, data=value)
+        print("Data saved to " + self.path + "data.h5")
+
+        # save config
+        # I should find a better way to do this
+        with open(self.path + "cfg.json", "w") as cfg_file:
+            json.dump(self.cfg, cfg_file, indent=4)
+        print("Config saved to " + self.path + "cfg.json")
+        
+        
         
         
 
--- Libraries/resonator_spectroscopy.py
+++ Libraries/resonator_spectroscopy.py
@@ -1,21 +1,3 @@
-"""
-Author: Charles Snipp
-Last Updated: 10/21/24
-
-This class defines programs that can be run to determine the time of flight
-of output pulses in order to calibrate the ADC trigger offset
-
-cfg.expt = {
-    "n_avg": 500,
-    "ro_len": 13,
-    "pulse_len": 7,
-    "freq": 5981.7,
-    "phase": 0,
-    "gain": 1,
-    "trig_offset": 0
-}
-
-"""
 from slab import experiment, AttrDict
 from slab.experiment import Experiment # ?
 from qick.pyro import make_proxy
@@ -25,9 +7,11 @@ import os, h5py, json
 import numpy as np
 import matplotlib.pyplot as plt
 
-# for tProc-configred outputs
-class tof_pulse(AveragerProgramV2):
+
+# a tProc configured ADC channel is required here
+class res_spec_pulse(AveragerProgramV2):
     def _initialize(self, cfg):
+        print("Initializing")
         cfg = AttrDict(cfg)
         
         ro_ch = cfg.soc.ro_ch
@@ -38,10 +22,18 @@ class tof_pulse(AveragerProgramV2):
         phase = cfg.expt.phase
         gain = cfg.expt.gain
         trig_offset = cfg.expt.trig_offset
+        pts = cfg.expt.pts
+
+        print("Config is loaded")
         
         self.declare_gen(ch=gen_ch, nqz=1)
         self.declare_readout(ch=ro_ch, length=ro_len)
+
+        self.add_loop(name='freq_loop', count=pts)
+    
         self.add_readoutconfig(ch=ro_ch, name='ro', freq=freq, gen_ch=gen_ch)
+
+        print("Readout is configured")
 
         self.add_pulse(ch=gen_ch, name="pulse", ro_ch=ro_ch, 
                        style="const", 
@@ -51,21 +43,24 @@ class tof_pulse(AveragerProgramV2):
                        gain=gain, 
                       )
 
-    def _body(self, cfg):
+        print("Done initializing")
 
+    def _body(self, cfg):
+        print("line 49")
         cfg = AttrDict(cfg)
         ro_ch = cfg.soc.ro_ch
         gen_ch = cfg.soc.res_gen_ch
         trig_offset = cfg.expt.trig_offset
-        
+        print("line 54")
         self.send_readoutconfig(ch=ro_ch, name='ro', t=0)
         self.trigger(ros=[ro_ch], pins=[0], t=trig_offset, ddr4=True)
         self.pulse(ch=gen_ch, name="pulse", t=0)
+        print("line 58")
         
 
-class time_of_flight(Experiment):
+class resonator_spectroscopy(Experiment):
     # import config file, specify data path
-    def __init__(self, path='', prefix='time_of_flight', config_file=None, liveplot_enabled=True, **kwargs):
+    def __init__(self, path='', prefix='resonator_spectroscopy', config_file=None, liveplot_enabled=True, **kwargs):
         super().__init__(path=path, prefix=prefix, config_file=config_file, liveplot_enabled=liveplot_enabled, **kwargs)
         
     # run the experiment
@@ -75,31 +70,31 @@ class time_of_flight(Experiment):
         ns_address = cfg.instrument_manager.ns_address
         ns_port = cfg.instrument_manager.ns_port
         proxy_name = cfg.instrument_manager.proxy_name
+        n_avg = cfg.expt.n_avg
+        fs = cfg.expt.fs
 
         soc, soccfg = make_proxy(ns_host=ns_address, ns_port=ns_port, proxy_name=proxy_name)
         
-        prog = tof_pulse(soccfg=soccfg, reps=1, final_delay=None, cfg=cfg)
-        iq_list = prog.acquire_decimated(soc, soft_avgs=cfg.expt.n_avg)
-        t = prog.get_time_axis(ro_index=0)
-        data = {"I": iq_list[0][:,0], "Q": iq_list[0][:,1], "time": t}
+        prog = res_spec_pulse(soccfg=soccfg, reps=n_avg, final_delay=None, cfg=cfg)
+        print("acquiring")
+        iq_list = prog.acquire(soc)
+        print("done acquiring")
+        data = {"I": iq_list[0][:,0], "Q": iq_list[0][:,1], "f": fs}
         self.data = data
         return data
 
     # plot results
     def display(self, save=True):
         data = self.data
-        t = data["time"]
+        fs = data["fs"]
         i = data["I"]
         q = data["Q"]
         mag = np.abs(i + 1j * q)
         fig = plt.figure(figsize=(9,7))
-        plt.plot(t, mag, label="magnitude")
-        plt.plot(t, i, label="i")
-        plt.plot(t, q, label="q")
-        plt.legend()
+        plt.plot(fs, mag)
         plt.ylabel("a.u.")
-        plt.xlabel("us")
-        plt.title("Time of Flight\nFrequency: " + str(self.cfg.expt.freq))
+        plt.xlabel("MHz")
+        plt.title("Resonator Spectroscopy")
         plt.show()
         if save:
             if not os.path.exists(self.path):
@@ -124,11 +119,4 @@ class time_of_flight(Experiment):
         with open(self.path + "cfg.json", "w") as cfg_file:
             json.dump(self.cfg, cfg_file, indent=4)
         print("Config saved to " + self.path + "cfg.json")
-        
-        
-        
-        
 
-
-
-    
